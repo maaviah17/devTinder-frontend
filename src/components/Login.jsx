112a6b2709ae10e77809cf05b363094e0cf2e084
Muawiyah <muawiyah1705@gmail.com>
2025-06-19 11:49:39 +0300

    fetching endpoints

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,91 @@
+import { useState } from "react"
+import axios from "axios";
 
 const Login = () => {
+
+    const [emailId, setEmailId] = useState("maaviah@mail.com");
+    const [password, setPassword] = useState("Maaviah123!!");
+
+    const handleLogin = async () => {
+        try {
+            const res = await axios.post("http://localhost:4000/login", {
+                emailId,
+                password
+            }, { withCredentials: true });
+            console.log(res);
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
+
     return (
-        <div>Login Page this is </div>
+        <div className="flex justify-center my-10">
+            <div className="card bg-base-300 w-96 shadow-sm">
+                <div className="card-body">
+                    <h2 className="card-title justify-center">Login</h2>
+                    <div>
+                        <span className="label label-text">Email ID</span>
+                        <label className="input validator my-2 ">
+
+                            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <g
+                                    strokeLinejoin="round"
+                                    strokeLinecap="round"
+                                    strokeWidth="2.5"
+                                    fill="none"
+                                    stroke="currentColor"
+                                >
+                                    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                                    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                                </g>
+                            </svg>
+
+                            <input type="email" className="text-white" value={emailId} placeholder="mail@site.com" required
+                                onChange={(e) => setEmailId(e.target.value)}
+                            />
+                        </label>
+                        <div className="validator-hint hidden">Enter valid email address</div>
+
+                        {/* password field */}
+                        <span className="label label-text mt-2">Password</span>
+                        <label className="input validator my-2">
+                            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <g
+                                    strokeLinejoin="round"
+                                    strokeLinecap="round"
+                                    strokeWidth="2.5"
+                                    fill="none"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"
+                                    ></path>
+                                    <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+                                </g>
+                            </svg>
+                            <input
+                                type="password"
+                                required
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
+                                placeholder="Password"
+                                minlength="8"
+                                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                                title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+                            />
+                        </label>
+                        <p className="validator-hint hidden">
+                            Must be more than 8 characters, including
+                            <br />At least one number <br />At least one lowercase letter <br />At least one uppercase letter
+                        </p>
+                    </div>
+                    <div className="card-actions justify-center m-2">
+                        <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
